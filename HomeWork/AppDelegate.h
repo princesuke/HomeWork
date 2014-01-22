bca720c80f9c0b1d47355af8fb560e969abdba15
Worawit.N <kingmaster09@gmail.com>
2014-01-22 12:20:37 +0700

    Initial Commit

---
+++ HomeWork/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HomeWork
+//
+//  Created by Mobile's iMac on 1/22/2557 BE.
+//  Copyright (c) 2557 Mobile's iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HomeWork/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HomeWork
+//
+//  Created by Mobile's iMac on 1/22/2557 BE.
+//  Copyright (c) 2557 Mobile's iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
